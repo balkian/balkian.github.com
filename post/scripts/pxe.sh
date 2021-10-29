0e0371c8e528e9f83700d6accc5434295f9e962c
J. Fernando Sánchez <balkian@gmail.com>
2021-10-29 20:17:28 +0200

    rebuilding site vie 29 oct 2021 20:17:28 CEST

--- post/scripts/pxe.sh
+++ post/scripts/pxe.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 USER=j
 IFNAME=enp62s0u1u3
-BINARY=test.bin
+BINARY=test-tftp.bin
 ip address flush dev $IFNAME
 ip address add 10.1.1.10/24 dev $IFNAME
 dnsmasq -i $IFNAME --dhcp-range=10.1.1.50,10.1.1.100 \
